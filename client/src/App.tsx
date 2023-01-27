666b7ae3fc5fcbd750aec9f892a592e261589268
dovakiin0 <subedibs@outlook.com>
2023-01-27 21:13:26 +0545

    bugnet in process: frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {Routes, Route} from "react-router-dom";
+import './App.css';
+import {ThemeProvider, createTheme, Theme} from "@mui/material/styles"
+
+import Layout from "./Layouts/Layout";
+
+const bugNetTheme : Theme = createTheme({
+  palette: {
+    mode: "dark"
+  }
+})
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ThemeProvider theme={bugNetTheme}>
+    <Routes>
+      <Route path="/" element={<Layout/>} />
+    </Routes>
+    </ThemeProvider>
   )
 }
 
---
+++ client/src/Layouts/Layout.tsx
@@ -0,0 +1,12 @@
+import {Typography} from "@mui/material"
+
+type Props = {}
+
+function Layout({}: Props) {
+  return (
+    
+    <Typography variant="h3">Home</Typography>
+  )
+}
+
+export default Layout
