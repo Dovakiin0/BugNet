f3608a47188ccd822c1ccc1eea0123ec9026fa23
dovakiin0 <subedibs@outlook.com>
2023-02-16 19:18:31 +0545

    test env setup for jest

--- server/src/controllers/project.controller.ts
+++ server/src/controllers/project.controller.ts
@@ -1,14 +1,34 @@
 import { Request, Response } from "express";
+import prisma from "../helper/prismaClient";
 
-const getAllProjects = async (req: Request, res: Response) => {};
+const getAllProjects = async (req: Request, res: Response) => {
+  try {
+    const projects = await prisma.project.findMany();
+    if (projects.length <= 0) {
+      return res.status(404).json({ message: "No projects found" });
+    }
+    return res.status(200).json(projects);
+  } catch (err) {
+    res.status(400).send(JSON.stringify(err));
+  }
+  return res.status(200).json([]);
+};
 
-const getProjectById = async (req: Request, res: Response) => {};
+const getProjectById = async (req: Request, res: Response) => {
+  return res.status(200).json({ message: "Hello World" });
+};
 
-const createProject = async (req: Request, res: Response) => {};
+const createProject = async (req: Request, res: Response) => {
+  return res.status(200).json({ message: "Hello World" });
+};
 
-const updateProject = async (req: Request, res: Response) => {};
+const updateProject = async (req: Request, res: Response) => {
+  return res.status(200).json({ message: "Hello World" });
+};
 
-const deleteProject = async (req: Request, res: Response) => {};
+const deleteProject = async (req: Request, res: Response) => {
+  return res.status(200).json({ message: "Hello World" });
+};
 
 export {
   getAllProjects,
--- server/src/test/project.test.ts
+++ server/src/test/project.test.ts
@@ -1,8 +1,15 @@
 import supertest from "supertest";
 import app from "../index";
 
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.test" });
+
 describe("Project Controller", () => {
-  it.todo("Should fetch all projects");
+  it("Should fetch all projects", async () => {
+    const res = await supertest(app).get("/api/v1/projects");
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
   it.todo("Should fetch a project by id");
   it.todo("Should create a projects");
   it.todo("Should update a projects");
