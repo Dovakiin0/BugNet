a56950bbc33b62eb2377ab957f82cdb429a91787
dovakiin0 <subedibs@outlook.com>
2023-03-16 17:10:57 +0545

    ready for deploy

---
+++ dist/assets/index-445988ab.js
@@ -0,0 +1,1 @@
+import{a9 as t,r as o,j as r}from"./index-5dcf5472.js";import{B as a}from"./chunk-6CSUKJP7-77e68efb.js";function s(){const{search:e}=t();return o.useMemo(()=>new URLSearchParams(e),[e])}function u(){const e=s();return window.opener.authenticateCallback(e.get("token")),window.close(),r.jsx(a,{children:"Success"})}export{u as default};
---
+++ dist/assets/useOauth-50523859.js
@@ -0,0 +1,1 @@
+function a(t){window.authenticateCallback=o=>{localStorage.setItem("token",o),window.location.reload()},window.open(t,"_blank","width=500, height=600")}const e=()=>({call:a});export{e as u};
