e42862a8dd4a7528d825043fa44d8cee7e17a13b
dovakiin0 <subedibs@outlook.com>
2023-05-04 20:56:35 +0545

    env mis

---
+++ dist/assets/index-e30a44f0.js
@@ -0,0 +1,1 @@
+import{a9 as t,r as o,j as r}from"./index-19d50d2a.js";import{B as a}from"./chunk-6CSUKJP7-be19e9e3.js";function s(){const{search:e}=t();return o.useMemo(()=>new URLSearchParams(e),[e])}function u(){const e=s();return window.opener.authenticateCallback(e.get("token")),window.close(),r.jsx(a,{children:"Success"})}export{u as default};
